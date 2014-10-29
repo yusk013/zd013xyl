ef35199f85c617306149c4858bc19e759c9ffe50
yusk013 <yusk013@126.com>
2014-10-29 08:54:39 +0800

    初始化express

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require('http');
 var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.listen(port);
