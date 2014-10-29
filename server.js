5b5108227c01745525bf735ba6f71bcc342d3e2a
yusk013 <yusk013@126.com>
2014-10-29 09:53:07 +0800

    初始化首页

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var http = require('http');
 var port = 18080;
+var path = require('path');
 
-var express = require('express');
+var express = require('express'),
+    exphbs = require('express-handlebars');
+
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.render('index', { layout: false });
 });
 
 app.listen(port);
